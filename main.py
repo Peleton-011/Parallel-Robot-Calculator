e2205571d873b828e862d3ae91e45826341a6e31
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2022-05-17 21:30:30 +0200

    Create main.py
    Added input function, initial variable declarations, and declared the initial calculation function (WIP)

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#Physical parameters
+
+P = 0 #Length of the effector platform
+Cx = 0 #X coord of the left shoulder
+Cy = 0 #Y coord of the left shoulder
+lb = 0 #Length of the upper half of the arm
+la = 0 #Length of the lower half of the arm
+D = 0 #Distance between shoulders
+Xl = 0 #X coord of the end of the upper half of the left arm
+Xr = 0 #X coord of the end of the upper half of the right arm
+Yl = 0 #Y coord of the end of the upper half of the left arm
+Yr = 0 #Y coord of the end of the upper half of the right arm
+
+#Purely calculational variables
+
+A = 0
+W = 0
+U = 0
+T = 0
+I = 0
+E = 0
+V = 0
+N = 0
+
+    # TODO: write code...
