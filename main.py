a203abaf64fa4592aef280368d13c6abf7d11b45
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2022-05-17 21:32:30 +0200

    Actually add the functions in the last commit
    The last commit was of the wrong file, this one should fix that

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import math
+
 #Physical parameters
 
 P = 0 #Length of the effector platform
@@ -7,10 +9,6 @@ Cy = 0 #Y coord of the left shoulder
 lb = 0 #Length of the upper half of the arm
 la = 0 #Length of the lower half of the arm
 D = 0 #Distance between shoulders
-Xl = 0 #X coord of the end of the upper half of the left arm
-Xr = 0 #X coord of the end of the upper half of the right arm
-Yl = 0 #Y coord of the end of the upper half of the left arm
-Yr = 0 #Y coord of the end of the upper half of the right arm
 
 #Purely calculational variables
 
@@ -23,4 +21,37 @@ E = 0
 V = 0
 N = 0
 
-    # TODO: write code...
+#Gets the unit in which the input will be given
+def getUnit():
+    unit = input("Select the unit for angles (d/r): ")
+    if unit == "d":
+        return 0
+    elif unit == "r":
+        return 1
+    else:
+        return getUnit()
+
+#Gets the input angles from the user
+def getInput():
+    mode = getUnit()
+    auxText = ["(degrees)", "(radians)"]
+    leftAngle = float(input("Insert the angle of the left motor " + auxText[mode]))
+    rightAngle = float(input("Insert the angle of the right motor " + auxText[mode]))
+    return [leftAngle, rightAngle]
+
+def initialCalculations():
+    angles = getInput()
+    
+    #X coord of the end of the upper half of the left arm
+    Xl = 0 
+    
+    #X coord of the end of the upper half of the right arm
+    Xr = 0 
+    
+    #Y coord of the end of the upper half of the left arm
+    Yl = 0 
+    
+    #Y coord of the end of the upper half of the right arm
+    Yr = 0 
+
+    
