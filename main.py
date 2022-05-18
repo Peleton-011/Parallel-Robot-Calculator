03843911ecbdafa67af8e6e391ca49aeb73d6169
Peleton-011 <103774269+Peleton-011@users.noreply.github.com>
2022-05-18 16:08:28 +0200

    Add final calculations and organization tweaks
    Initial calculations are still missing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import sys
 import math
 
 #Physical parameters
@@ -10,16 +11,6 @@ lb = 0 #Length of the upper half of the arm
 la = 0 #Length of the lower half of the arm
 D = 0 #Distance between shoulders
 
-#Purely calculational variables
-
-A = 0
-W = 0
-U = 0
-T = 0
-I = 0
-E = 0
-V = 0
-N = 0
 
 #Gets the unit in which the input will be given
 def getUnit():
@@ -32,15 +23,19 @@ def getUnit():
         return getUnit()
 
 #Gets the input angles from the user
-def getInput():
-    mode = getUnit()
+def getInput(mode = sys.maxsize):
+    #Get the params if they haven't been provided
+    if mode > 1:
+        mode = getUnit()
     auxText = ["(degrees)", "(radians)"]
     leftAngle = float(input("Insert the angle of the left motor " + auxText[mode]))
     rightAngle = float(input("Insert the angle of the right motor " + auxText[mode]))
     return [leftAngle, rightAngle]
 
-def initialCalculations():
-    angles = getInput()
+def initialCalculations(angles = []):
+    #Get the params if they haven't been provided
+    if not angles:
+        angles = getInput()
     
     #X coord of the end of the upper half of the left arm
     Xl = 0 
@@ -52,6 +47,50 @@ def initialCalculations():
     Yl = 0 
     
     #Y coord of the end of the upper half of the right arm
-    Yr = 0 
-
+    Yr = 0
     
+    #Purely calculational variables
+
+    A = 0
+    W = 0
+    U = 0
+    T = 0
+    I = 0
+    E = 0
+    V = 0
+    N = 0
+    
+    return [A, W, U, T, I, E, V, N]
+
+#Main and final calculations
+def mainCalculations(coordsAndParams = []):
+    #Get the params if they haven't been provided
+    if not coordsAndParams:
+        coordsAndParams = initialCalculations()
+    
+    #Unpacking variables
+    A = coordsAndParams[0]
+    W = coordsAndParams[1]
+    U = coordsAndParams[2]
+    T = coordsAndParams[3]
+    I = coordsAndParams[4]
+    E = coordsAndParams[5]
+    V = coordsAndParams[6]
+    N = coordsAndParams[7]
+    
+    #Auxiliary variable calculations
+    auxA = (W *  E) + (W * T * V) - (T * I)
+    auxB = (I * U) - (A * E) - (A * U * N)
+    auxDenominator = (W * U) - (A * T)
+    auxNumeratorA1 = (W * U * N) + auxA
+    auxNumeratorB1 = auxB - (A * T * V)
+    auxNumeratorA2 = (A * T * N) + auxA
+    auxNumeratorB2 = auxB - (W * U * V)
+    
+    #Actual calculations according to some predefined equations
+    a1 = auxNumeratorA1/auxDenominator
+    b1 = auxNumeratorB1/auxDenominator
+    a2 = auxNumeratorA2/auxDenominator
+    b2 = auxNumeratorB2/auxDenominator
+
+    return [a1, b1, a2, b2]
